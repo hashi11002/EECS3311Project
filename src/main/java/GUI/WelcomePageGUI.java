506fa8c434dc33f093e724a978794577672fe0d9
aisha895 <aisha895@my.yorku.ca>
2023-11-19 22:20:23 -0500

    Welcomepage changed

--- src/main/java/GUI/WelcomePageGUI.java
+++ src/main/java/GUI/WelcomePageGUI.java
@@ -1,143 +1,52 @@
 package GUI;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.*;
-import java.util.prefs.Preferences;
 
-public class WelcomePageGUI extends JFrame implements ActionListener {
-	public static void main(String[] args) {
-		
-	WelcomePageGUI welcomePage = new WelcomePageGUI();
-	}
+public class WelcomePageGUI extends JFrame {
 
-	JFrame frame;
-	JLabel titleLabel = new JLabel("Welcome to The Warehouse Management System! "
-			+ " To get started, please login into your account!");
-	JLabel userLabel = new JLabel("Enter Username:");
-	JLabel passwordLabel = new JLabel("Enter Password:");
-	JTextField userTextField = new JTextField();
-	JPasswordField passwordField = new JPasswordField();
-	JButton loginButton = new JButton("LOGIN");
-	JButton resetButton = new JButton("RESET");
-	JButton backButton = new JButton("BACK");
-	JCheckBox showPassword = new JCheckBox("Show Password");
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            WelcomePageGUI welcomePage = new WelcomePageGUI();
+        });
+    }
 
+    public WelcomePageGUI() {
+        setTitle("Welcome Page");
+        setSize(300, 150);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	WelcomePageGUI()
-	{
-		//Calling methods inside constructor.
-		createWindow();
-		setLocationAndSize();
-		addComponentsToContainer();
-		addActionEvent();
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2, 1));
 
-	}
+        JButton adminButton = createButton("Admin Login", this::openAdminLogin);
+        JButton clientButton = createButton("Client Ordering", this::openClientOrdering);
 
-	public void createWindow()
-	{
-		frame = new JFrame();
-		frame.setTitle("The warehouse system ");
+        panel.add(adminButton);
+        panel.add(clientButton);
 
-		frame.setBounds(100,100,900,750);
-		//frame.getContentPane().setBackground(Color.pink);
-		frame.getContentPane().setLayout(null);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(true);
-	}
+        add(panel);
+        setLocationRelativeTo(null);
+        setVisible(true); 
+    }
 
-	public void setLocationAndSize()
-	{
-		//Setting location and Size of each components using setBounds() method.
-		// setBounds (x, y, width, height)
-		titleLabel.setBounds(100,50,3000,30);
-		userLabel.setBounds(50,150,150,30);
-		passwordLabel.setBounds(50,220,100,30);
-		userTextField.setBounds(175,150,150,30);
-		passwordField.setBounds(175,220,150,30);
-		showPassword.setBounds(150,250,150,30);
-		loginButton.setBounds(50,300,100,30);
-		resetButton.setBounds(200,300,100,30);
-		backButton.setBounds(125,350,100,30);
+    private JButton createButton(String text, ActionListener listener) {
+        JButton button = new JButton(text);
+        button.addActionListener(listener);
+        return button;
+    }
 
+    private void openAdminLogin(ActionEvent e) {
+        LoginGUI adminLoginGUI = new LoginGUI();
+        adminLoginGUI.setVisible(true);
+        dispose(); 
+    }
 
-	}
-
-	public void addComponentsToContainer()
-	{
-		//Adding each components to the Container
-		frame.add(titleLabel);
-		frame.add(userLabel);
-		frame.add(passwordLabel);
-		frame.add(userTextField);
-		frame.add(passwordField);
-		frame.add(showPassword);
-		frame.add(loginButton);
-		frame.add(resetButton);
-		frame.add(backButton);
-	}
-
-	public void addActionEvent()
-	{
-		//adding Action listener to components
-		loginButton.addActionListener(this);
-		resetButton.addActionListener(this);
-		showPassword.addActionListener(this);
-		backButton.addActionListener(this);
-	}
-
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-
-		//Coding Part of LOGIN button
-		if (e.getSource() == loginButton) {
-			try {
-				Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/User_Data","root","");
-				String userText;
-				String pwdText;
-				userText = userTextField.getText();
-				pwdText = passwordField.getText();
-				PreparedStatement ps= connection.prepareStatement("select Login_info.password from Login_Info where Login_Info.username =?");
-				ps.setString(1, userText);
-				ResultSet rs2 = ps.executeQuery();
-				rs2.next();
-				if (rs2 != null && rs2.getString(1).equals(pwdText)) {
-					//JOptionPane.showMessageDialog(this, "Login Successful");
-					frame.dispose();
-					//Preferences nWindow = new Preferences();
-				} else {
-					JOptionPane.showMessageDialog(this, "Invalid Username or Password");
-				}
-
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			}
-		}
-
-		//Coding Part of RESET button
-		if (e.getSource() == resetButton) {
-			userTextField.setText("");
-			passwordField.setText("");
-		}
-
-		//Coding Part of showPassword JCheckBox
-		if (e.getSource() == showPassword) {
-			if (showPassword.isSelected()) {
-				passwordField.setEchoChar((char) 0);
-			} else {
-				passwordField.setEchoChar('*');
-			}
-		}
-
-		if (e.getSource() == backButton) {
-			frame.dispose();
-			LoginGUI previousWindow = new LoginGUI();
-		}
-	}
+    private void openClientOrdering(ActionEvent e) {
+        PlacingOrderGUI clientOrdering = new PlacingOrderGUI();
+        clientOrdering.setVisible(true);
+        dispose(); 
+    }
 }
-
-
-	  
