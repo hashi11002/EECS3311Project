c700f7fa001cbb0783063d0d5b7baf45a323fb88
aisha895 <aisha895@my.yorku.ca>
2023-12-03 20:22:42 -0500

    delete product updated in updater

--- src/main/java/Model/Updater.java
+++ src/main/java/Model/Updater.java
@@ -7,50 +7,61 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Updater {
-	
+
 	static String url = "jdbc:mysql://localhost:3306/3311ProjectDatabase";
 	static String user = "root";
 	static String password = "";
-	
+
 	public static void updateProduct(String ProductName, int restockQuantity) {
 		/* TO BE IMPLEMENTED */
-			
-	        try {
-	           
-	        	Connection con = DriverManager.getConnection(url, user, password);
-				String query = "select * from product where itemName = ?";
-				PreparedStatement ps = con.prepareStatement(query);
-				ps.setString(1, ProductName);
-				ResultSet rs = ps.executeQuery();
-				
-	            if (rs.next()) {
-	
-	                int currentStock = rs.getInt("remainingStock");
-	                int newStock = currentStock + restockQuantity;
 
-	           
-	                String updateQuery = "UPDATE product SET remainingStock = ? WHERE productID = ?";
-	                ps = con.prepareStatement(updateQuery);
-	                ps.setInt(1, newStock);
-	                ps.setString(2, ProductName);
+		try {
 
-	                
-	                ps.executeUpdate();
-	                System.out.println("Product updated: " +  ProductName);
-	            } else {
-	                System.out.println("Product not found: " +  ProductName);
-	            }
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	        } 
-	         
-	           
-	        }
-	    
-	
-	
-	public static void depleteProduct(String ProductID, int depleteQuantity) {
+			Connection con = DriverManager.getConnection(url, user, password);
+
+			String updateQuery = "UPDATE product SET remainingStock = remainingStock + ? WHERE productID = ?";
+			PreparedStatement ps = con.prepareStatement(updateQuery);
+			ps.setInt(1, restockQuantity);
+			ps.setString(2,ProductName);
+			ResultSet rs = ps.executeQuery();
+
+
+			int affectedRows = ps.executeUpdate();
+			if (affectedRows > 0) {
+				System.out.println("Product stock updated: " + ProductName);
+			} else {
+				System.out.println("Product not found or no change in stock: " + ProductName);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+
+
+	public static void depleteProduct(String ProductName, int depleteQuantity) {
 		/* TO BE IMPLEMENTED */
+		try {
+
+			Connection con = DriverManager.getConnection(url, user, password);
+
+
+			String updateQuery = "UPDATE product SET remainingStock = GREATEST(0, remainingStock - ?) WHERE productID = ?";
+			PreparedStatement ps = con.prepareStatement(updateQuery);
+			ps.setInt(1, depleteQuantity);
+			ps.setString(2, ProductName);
+
+
+			int affectedRows = ps.executeUpdate();
+			if (affectedRows > 0) {
+				System.out.println("Product stock decreased: " + ProductName);
+			} else {
+				System.out.println("Product not found or no change in stock: " + ProductName);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
